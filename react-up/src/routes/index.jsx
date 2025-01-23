04c92e3c487850141bd6ea0f1fded4ee43c41f1c
Ronnyvilla3021 <ronnievillavilla@gmail.com>
2025-01-22 21:29:23 -0500

    react

--- react-up/src/routes/index.jsx
+++ react-up/src/routes/index.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import Dashboard from '../pages/Dashboard';
+import Dashboard from '../pages/Admin/Dashboard';
 
 const AppRoutes = () => {
   return (
